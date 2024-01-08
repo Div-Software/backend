e190a531bdbd6531ad178be876cd9758b30620ea
Adrian Rincon <adrianrincon102001@gmail.com>
2024-01-08 13:13:20 -0400

    cambios

--- api/app.py
+++ api/app.py
@@ -131,7 +131,8 @@ def send_contact_email(name, email, message, source=None,):
 
 mail.init_app(app)
 
-if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
